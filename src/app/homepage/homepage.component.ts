b533f89c91a9243f81c8a1642d5a1ffb14428e14
manishsingh2002 <msms5476mm@gmail.com>
2024-03-29 15:19:56 +0530

    manish 29/03

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -21,6 +21,8 @@ export class HomepageComponent {
         response.products.forEach((element: any) => {
           this.productdata.push(element);
         });
+      } else {
+        console.log('Error fetching data or no products in response');
       }
     });
   }
@@ -40,26 +42,28 @@ export class HomepageComponent {
     this.filteredProducts.length = 0;
     this.toggle = true;
   }
-
   filtering() {
     this.filteredProducts = this.productdata.filter((product: any) => {
-      // let match = true;
-
-      if (this.filteredData.categories.length > 0) {
-        return this.filteredData.categories.includes(product.category);
+      console.log(
+        'nehalllllllllllllllllllll',
+        this.filteredData.category.length
+      );
+      if (this.filteredData.category.length > 0) {
+        return this.filteredData.category.includes(product.category);
       }
 
-      if (this.filteredData.brands.length > 0) {
-        return this.filteredData.brands.includes(product.brand);
+      if (this.filteredData.brand.length > 0) {
+        return this.filteredData.brand.includes(product.brand);
       }
+      console.log('filtered data aaaaaaaaaaaa', this.filteredData); // Check if filteredData is populated here
 
-      if (this.filteredData.ratings.length > 0) {
+      if (this.filteredData.rating.length > 0) {
         const minrating = this.filteredData.ratings[0];
         const maxrating = this.filteredData.ratings[1] || Infinity;
         return product.rating >= minrating && product.rating <= maxrating;
       }
 
-      if (this.filteredData.prices.length > 0) {
+      if (this.filteredData.price.length > 0) {
         // Assuming filteredData.prices is an array of numbers representing min and max prices
         const minPrice = this.filteredData.prices[0];
         const maxPrice = this.filteredData.prices[1] || Infinity; // Handle single price or range
@@ -79,25 +83,3 @@ export class HomepageComponent {
     console.log(this.cartData);
   }
 }
-// }
-// filtering() {
-//   this.filteredProducts = this.productdata.filter((product: any) => {
-//     if (this.filteredData.categories.length > 0) {
-//       return this.filteredData.categories.includes(product.category);
-//     }
-//     if (this.filteredData.brands.length > 0) {
-//       return this.filteredData.brands.includes(product.brand);
-//     }
-
-//     if (this.filteredData.ratings.length > 0) {
-//       return this.filteredData.ratings >= product.rating;
-//     }
-
-//     if (this.filteredData.prices.length > 0) {
-//       return this.filteredData.price.includes(
-//         product.price > 0 && product.price <= this.filteredData.price
-//       );
-//     }
-//   });
-//   console.log('Filtered Products:', this.filteredProducts);
-// }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,14 +14,19 @@ import { DataService } from '../data.service';
 })
 export class NavbarComponent implements OnInit {
   public data: any;
+  // basic categories storing data
   public category: any[] = [];
   public brand: any[] = [];
   public price: any[] = [];
   public rating: any[] = [];
+  //
+  // selectedd filtered out data after searching for a category
   selectedCategories: string[] = [];
   selectedRatings: number[] = [];
   selectedBrands: string[] = [];
   selectedPrices: any = [];
+  //
+  //
   togglecateegories = false;
   togglebrandings = false;
   toggleratings = false;
@@ -29,45 +34,41 @@ export class NavbarComponent implements OnInit {
   productdata: any[] = [];
   selectedcategorybrands: any = [];
   // input data for child elements
-  public filtereddata: { [key: string]: any[] } = {
-    categories: this.selectedCategories,
-    brands: this.selectedBrands,
-    prices: this.selectedPrices,
-    ratings: this.selectedRatings,
+  public filteredData: { [key: string]: string[] | number[] } = {
+    category: this.selectedCategories,
+    brand: this.selectedBrands,
+    price: this.selectedPrices,
+    rating: this.selectedRatings,
   };
-  // toggling
 
-  //
+  // toggling
   constructor(private dataService: DataService) {}
 
+  // main filder where we are storing the categories names in the theeir array
   ngOnInit() {
     this.dataService.getData().subscribe((response) => {
       if (response && response.products) {
         response.products.forEach((element: any) => {
           if (!this.category.includes(element.category)) {
             this.category.push(element.category);
+            // console.log('cateegory', this.category);
           }
           if (!this.brand.includes(element.brand)) {
             this.brand.push(element.brand);
+
+            // console.log('brand', this.brand);
           }
           if (!this.price.includes(element.price)) {
             this.price.push(element.price);
+            // console.log('price', this.price);
           }
           if (!this.rating.includes(element.rating)) {
             this.rating.push(element.rating);
+            // console.log('rating', this.rating);
           }
         });
       }
     });
-    //
-    this.dataService.getData().subscribe((response) => {
-      if (response && response.products) {
-        response.products.forEach((element: any) => {
-          this.productdata.push(element);
-        });
-      }
-    });
-    //
   }
   ////////////////////////////////////////////////////////////////////////////////////
   togglecategory() {
@@ -83,77 +84,70 @@ export class NavbarComponent implements OnInit {
     this.togglepricings = !this.togglepricings;
   }
 
-  ////////////////////////////////////////////////////////////////////////////////////
-  onPriceChange(event: any) {
-    const price = event.target.value;
-    const isChecked = event.target.checked;
+  ////////////////////////  /////// //////////////////////////////////////////////////////////\
 
+  onvalueChange(event: any) {
+    const name = event.target.name;
+    const value = event.target.value;
+    const isChecked = event.target.checked;
+    //
+
+    // console.log(value, 'manishdkjabdbwawd', name);
     if (isChecked) {
-      this.selectedPrices.push(price);
-    } else {
-      const index = this.selectedPrices.indexOf(price);
-      if (index !== -1) {
-        this.selectedPrices.splice(index, 1);
+      if (name === 'categories') {
+        this.selectedCategories.push(value);
       }
-    }
-    console.log('Selected prices:', this.selectedPrices);
-    // console.log(this.filtereddata); // For debugging or logging
-  }
-  ////////////////////////////////////////////////////////////////////////////////
-  oncategoryChange(event: any) {
-    const category = event.target.value;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      this.selectedCategories.push(category);
     } else {
-      const index = this.selectedCategories.indexOf(category);
+      const index = this.selectedCategories.indexOf(value);
       if (index !== -1) {
         this.selectedCategories.splice(index, 1);
       }
+      console.log('mmmmmmmmmmmmmmmmmmmmm', this.selectedCategories);
     }
-    console.log('seleccted categoris', this.selectedCategories); // For debugging or logging
 
-    this.filtereddata['brand'] = this.productdata.filter((category: any) => {
-      if (this.selectedCategories.length > 0) {
-        return this.selectedCategories.includes(category.category);
-      } else {
-        return true;
+    // for brand
+    if (name === 'brand') {
+      if (isChecked) {
+        this.selectedBrands.push(value);
       }
-    });
-  }
-
-  //////////  //////////////////////////////////
-  onratingChange(event: any) {
-    const rating = event.target.value;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      this.selectedRatings.push(rating);
     } else {
-      const index = this.selectedRatings.indexOf(rating);
-      if (index !== -1) {
-        this.selectedRatings.splice(index, 1);
-      }
-    }
-    // console.log(this.filtereddata); // For debugging or logging
-
-    console.log('seleccted rating', this.selectedRatings); // For debugging or logging
-  }
-  ///////////////// //  /////////////////// /////////////////// //////////////////
-  onbrandchange(event: any) {
-    const brand = event.target.value;
-    const isChecked = event.target.checked;
-
-    if (isChecked) {
-      this.selectedBrands.push(brand);
-    } else {
-      const index = this.selectedBrands.indexOf(brand);
+      const index = this.selectedBrands.indexOf(value);
       if (index !== -1) {
         this.selectedBrands.splice(index, 1);
       }
+      console.log('filteredData', this.filteredData);
     }
-    console.log('seleccted brand', this.selectedBrands);
-    // console.log(this.filtereddata['brands']); // For debugging or logging
+
+    //
+    // for pricce
+    if (name === 'price') {
+      if (isChecked) {
+        this.selectedPrices.push(value);
+      }
+    } else {
+      const index = this.selectedPrices.indexOf(value);
+      if (index !== -1) {
+        this.selectedPrices.splice(index, 1);
+      }
+      console.log('filteredData', this.filteredData);
+    }
+
+    //  ratinng
+    if (name === 'rating') {
+      if (isChecked) {
+        this.selectedRatings.push(value);
+      } else {
+        const index = this.selectedRatings.indexOf(value);
+        if (index !== -1) {
+          this.selectedRatings.splice(index, 1);
+        }
+        console.log('filteredDatajqvdbwbqbdwdhid', this.filteredData);
+      }
+      // //
+      // console.log('Selected prices:', name, this.selectedPrices);
+      // console.log('seleccted categoris', this.selectedCategories);
+      // console.log('seleccted rating', this.selectedRatings);
+      // console.log('seleccted brand', this.selectedBrands);
+    }
   }
 }
